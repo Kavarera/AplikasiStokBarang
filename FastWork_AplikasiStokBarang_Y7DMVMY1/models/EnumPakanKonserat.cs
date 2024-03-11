4cb32a139f12fc8397ea6a084fa2f8dcc5ca1d8d
Kavarera <rafli.030715@gmail.com>
2024-03-11 18:09:06 +0700

    first init

---
+++ FastWork_AplikasiStokBarang_Y7DMVMY1/models/EnumPakanKonserat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWork_AplikasiStokBarang_Y7DMVMY1.models
+{
+    public enum EnumPakanKonserat
+    {
+        KAMBING = 1,
+        SAPI = 2
+    }
+}
