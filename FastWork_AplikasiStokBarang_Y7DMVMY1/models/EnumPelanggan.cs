f35026f56cbd6d5751c5e5386b57f79be0cd4959
Kavarera <rafli.030715@gmail.com>
2024-03-12 02:02:52 +0700

    add enumPelanggan.cs

---
+++ FastWork_AplikasiStokBarang_Y7DMVMY1/models/EnumPelanggan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWork_AplikasiStokBarang_Y7DMVMY1.models
+{
+    public enum EnumPelanggan
+    {
+        REGULAR=0,
+        ANGGOTA = 1
+    }
+}
